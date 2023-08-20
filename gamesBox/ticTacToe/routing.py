dbab4a722e48c57469450f19a762b4decf9152b7
Arisht-2000 <arishtjain2000@gmail.com>
2023-08-20 19:40:43 +0530

    fixed some routing issues

--- gamesBox/ticTacToe/routing.py
+++ gamesBox/ticTacToe/routing.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .consumers import AsyncGameBoardConsumer
 
 websocket_urlpatterns = [
-    path("ws/game/<int:game_id>/", AsyncGameBoardConsumer.as_asgi()),
+    re_path(r"ws/game/(?P<game_id>\d+)/$", AsyncGameBoardConsumer.as_asgi()),
+    # path("ws/game/<int:game_id>/", AsyncGameBoardConsumer.as_asgi()),
 ]
