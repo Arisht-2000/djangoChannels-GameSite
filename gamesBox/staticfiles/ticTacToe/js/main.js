952344ff0781aeef2db7cd6f2bb5cb205774c104
Arisht-2000 <arishtjain2000@gmail.com>
2023-08-20 16:02:47 +0530

    fixing bugs

---
+++ gamesBox/staticfiles/ticTacToe/js/main.js
@@ -0,0 +1,6 @@
+// myapp/static/myapp/js/main.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GameBoard from './GameBoard';  // Your React component
+
+ReactDOM.render(<GameBoard />, document.getElementById('root'));
---
+++ gamesBox/ticTacToe/webpack.config.js
@@ -0,0 +1,24 @@
+// E:\djangoChannels-GameSite\gamesBox\ticTacToe\webpack.config.js
+const path = require('path');
+
+module.exports = {
+    entry: './static/ticTacToe/js/main.js',  // Path to your main.js
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'static', 'ticTacToe', 'js'),  // Output path
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+};
