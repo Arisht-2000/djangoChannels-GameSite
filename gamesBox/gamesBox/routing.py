c2ab68b05a6af02298612c432123ccc009c80ae2
Arisht-2000 <arishtjain2000@gmail.com>
2023-08-20 14:02:40 +0530

    fixed ws routes

--- gamesBox/gamesBox/routing.py
+++ gamesBox/gamesBox/routing.py
@@ -1,6 +1,11 @@
-from django.urls import include, path
+from django.urls import path
+
+from channels.routing import URLRouter
+
+from ticTacToe import routing as ticTacToe_routing
+from chat import routing as chat_routing
 
 websocket_urlpatterns = [
-    path("ttt/", include("ticTacToe.routing")),
-    path("chat/", include("chat.routing")),
+    path("ttt/", URLRouter(ticTacToe_routing)),
+    path("chat/", URLRouter(chat_routing)),
 ]
--- gamesBox/ticTacToe/routing.py
+++ gamesBox/ticTacToe/routing.py
@@ -3,6 +3,5 @@ from django.urls import path
 from .consumers import AsyncGameBoardConsumer
 
 websocket_urlpatterns = [
-    # re_path(r"ws/game/(?P<game_id>\w+)/$", AsyncGameBoardConsumer.as_asgi()),
     path("ws/game/<int:game_id>/", AsyncGameBoardConsumer.as_asgi()),
 ]
